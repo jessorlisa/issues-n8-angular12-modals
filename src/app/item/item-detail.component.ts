6f4167e9864ae21eab92505b7a5f5d446c78411a
Jessica Johann <jessorlisa@gmail.com>
2021-07-08 10:24:48 +0200

    first modal

--- src/app/item/item-detail.component.ts
+++ src/app/item/item-detail.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import {Component, OnInit} from '@angular/core'
+import {ModalDialogParams} from '@nativescript/angular';
 
-import { Item } from './item'
-import { ItemService } from './item.service'
+import {Item} from './item'
+import {ItemService} from './item.service'
 
 @Component({
   selector: 'ns-details',
   templateUrl: './item-detail.component.html',
 })
 export class ItemDetailComponent implements OnInit {
-  item: Item
 
-  constructor(private itemService: ItemService, private route: ActivatedRoute) {}
+  itemId: number;
+  item: Item;
+
+  iterations: number[];
+
+  constructor(
+    private params: ModalDialogParams,
+    private itemService: ItemService
+  ) {
+    this.itemId = params.context;
+
+    this.iterations = Array(100).fill(0).map((x, i) => i+1);
+  }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.params.id
-    this.item = this.itemService.getItem(id)
+
+    this.item = this.itemService.getItem(this.itemId);
+  }
+
+  closeModal(): void {
+    this.params.closeCallback({
+      name: this.item.name
+    });
   }
 }
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnInit, ViewContainerRef} from '@angular/core'
+import {ModalDialogOptions, ModalDialogService} from "@nativescript/angular";
 
-import { Item } from './item'
-import { ItemService } from './item.service'
+import {Item} from './item'
+import {ItemService} from './item.service'
+import {ItemDetailComponent} from "./item-detail.component";
+
 
 @Component({
   selector: 'ns-items',
@@ -10,9 +13,49 @@ import { ItemService } from './item.service'
 export class ItemsComponent implements OnInit {
   items: Array<Item>
 
-  constructor(private itemService: ItemService) {}
+
+
+  constructor(
+    private modalService: ModalDialogService,
+    private vcRef: ViewContainerRef,
+    private itemService: ItemService
+  ) {
+  }
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+    this.items = this.itemService.getItems();
+  }
+
+
+  /**
+   * ************************************************************************
+   * Open detail modal
+   */
+
+  /**
+   *
+   */
+  openDetailModal(itemId: number): void {
+
+    this.createDetailModal(itemId).then(result => {
+
+      console.log('MODAL CLOSED:', result);
+
+    }).catch(error => console.log('ERROR', error));
+  }
+
+  /**
+   *
+   * @param itemId
+   */
+  private createDetailModal(itemId: number): Promise<any> {
+    const options: ModalDialogOptions = {
+      viewContainerRef: this.vcRef,
+      context: itemId,
+      fullscreen: true,
+    };
+
+    // showModal returns a promise with the received parameters from the modal page
+    return this.modalService.showModal(ItemDetailComponent, options);
   }
 }
